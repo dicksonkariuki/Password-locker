eba8f75104534d1101778e75e86c51e56577f2e9
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-06 20:01:07 +0300

    Create credentials class for account and user authentication

---
+++ password.py
@@ -0,0 +1,9 @@
+class Credentials:
+    """
+    class that creates a user's account and authentication information.
+    """
+    users_list = []
+    def__init__(self, authenticate, username, password):
+    """
+    __init__ method that helps define instances of the class.
+    """
