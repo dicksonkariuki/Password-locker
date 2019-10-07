d3999e93ace0b5016d271fe16080a02c6c0076e1
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-07 09:58:12 +0300

    create a test class that defines test cases for credentials class

--- password_test.py
+++ password_test.py
@@ -1,2 +1,9 @@
-import unnittest
-from password import Credentials, UsersInfo
+import unittest  # importing the unittest module
+import pyperclip  # importing the pyperclip module
+from password import Credentials, UsersInfo  # importing user defined classes
+
+
+class TestCredentials(unittest.TestCase):
+    """
+        Test class that defines the test cases for Credentials class.
+    """
